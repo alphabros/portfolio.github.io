59eb0495f979f53f8436fcd98ecdc26ed7c0d34f
alphabros <129158678+alphabros@users.noreply.github.com>
2023-03-28 16:44:46 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slick-carousel').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: 'linear'
+  });
+});
